0748c7a81aad70c3974fe329e9097120629c7af6
Michael Muehlemann <m.muehlemann@gmx.net>
2025-08-31 20:18:20 +0200

    added book path argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from stats import get_letter_stats, get_num_words, sort_letter_stats
+import sys
 
-PATH = "books/frankenstein.txt"
+from stats import get_letter_stats, get_num_words, sort_letter_stats
 
 
 def get_book_text(filepath):
@@ -10,12 +10,18 @@ def get_book_text(filepath):
 
 
 def main():
-    txt = get_book_text(PATH)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
+
+    txt = get_book_text(path)
     stats = get_letter_stats(txt)
     sorted_stats = sort_letter_stats(stats)
 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {PATH}...")
+    print(f"Analyzing book found at {path}...")
     print("----------- Word Count ----------")
     print(f"Found {get_num_words(txt)} total words")
     print("--------- Character Count -------")
